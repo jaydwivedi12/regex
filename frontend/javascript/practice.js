f451c1cceb3f44e526fa751b4b2db267076d5417
jaydwivedi12 <jayprakashdwivedi12@gmail.com>
2023-08-09 11:03:18 +0530

    DOM using id

---
+++ frontend/javascript/practice.js
@@ -0,0 +1,14 @@
+// var n = 100;
+// var i = 1;
+// while (i <= n) {
+
+//     if (i == n/2) {
+//         i++;
+//         continue;
+//     }
+//      if(i%2!=0)
+//     console.log(i + " odd");
+//     else
+//     console.log(i + " even");
+//     i++;
+// }
